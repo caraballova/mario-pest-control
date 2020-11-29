131deee942059298fae35979e1740108be004987
mollyembry <molly.embry@bryanuniversity.edu>
2020-11-29 17:22:48 -0500

    marios-pest-control

--- website.js
+++ website.js
@@ -1,4 +1,4 @@
-document.querySelector(".goomba2").addEventListener("input", function(){
+document.querySelector(".goomba2").addEventListener("keyup", function(){
     var goombaTotal = Number(document.querySelector(".goomba2").value * 5);
     var bobTotal = Number(document.querySelector(".bob2").value * 7);
     var cheepTotal = Number(document.querySelector(".cheep2").value * 11);
